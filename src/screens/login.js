9de853878a9b206fe2782a2d205b0ce28d91f6d5
maksymilianCzadowski <maksymilian.czadowski@gmail.com>
2022-12-17 17:23:23 +0100

    login

--- src/screens/login.js
+++ src/screens/login.js
@@ -1,13 +1,70 @@
 import React from 'react';
-import {View, Text, Button} from 'react-native';
+import {View, Text, TouchableOpacity} from 'react-native';
+import styled from 'styled-components';
 
 const Login = ({navigation}) => {
+  const [input, setInput] = React.useState({email: '', password: ''});
+
+  const logMeIn = () => {
+    if (input.email === 'admin' && input.password === 'admin') {
+      navigation.navigate('Home');
+    } else {
+      alert('Invalid Credentials');
+    }
+  };
+
+  const logGuest = () => {
+    navigation.navigate('Home');
+  };
+
   return (
     <View>
-      <Text>Login</Text>
-      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+      <View>
+        <TextInputContainer>
+          <TextInputStyled
+            value={input.email}
+            onChangeText={text => setInput({...input, email: text})}
+          />
+        </TextInputContainer>
+        <TextInputContainer>
+          <TextInputStyled
+            value={input.password}
+            onChangeText={text => setInput({...input, password: text})}
+            secureTextEntry={true}
+          />
+        </TextInputContainer>
+        <LoginButton onPress={logMeIn}>
+          <TextCenter>Login</TextCenter>
+        </LoginButton>
+        <TouchableOpacity onPress={logGuest}>
+          <TextCenter>Play as guest</TextCenter>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
+
+const LoginButton = styled.TouchableOpacity`
+  background-color: white;
+  padding: 12px;
+  border-radius: 4px;
+  margin: 10px;
+`;
+
+const TextCenter = styled.Text`
+  text-align: center;
+  color: #0f4c61;
+`;
+
+const TextInputContainer = styled.View`
+  margin: 10px;
+`;
+
+const TextInputStyled = styled.TextInput`
+  background-color: white;
+  padding: 12px;
+  border-radius: 12px;
+  color: #0f4c61;
+`;
 
 export default Login;
