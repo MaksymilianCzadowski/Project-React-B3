bbc2e2feffbc45cdf0dd711aaf431e1ac928bc16
Maksymilian Czadowski <maksymilian.czadowski@gmail.com>
2023-01-04 16:35:03 +0100

    logo patch by flo, merci frero

--- src/components/game/header.js
+++ src/components/game/header.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react';
 import {View, Text, Image} from 'react-native';
 import styled from 'styled-components';
 import ImageResizer from '@bam.tech/react-native-image-resizer';
+import {SafeAreaView} from 'react-native-safe-area-context';
 
 const Header = () => {
   const [imageUri, setImageUri] = useState(null);
@@ -22,23 +23,48 @@ const Header = () => {
   }, []);
 
   return (
-    <View>
-      {imageUri && (
-        <Image
-          source={{uri: imageUri}}
-          style={{width: newWidth, height: newHeight}}
+    <HeaderDiv>
+      <ViewDiv>
+        <HomeButt>{/* ici mettre le logo de home */}</HomeButt>
+        <LogoView
+          resizeMode="cover"
+          source={require('../../assets/img/tusmo-logo.png')}
         />
-      )}
-    </View>
+      </ViewDiv>
+    </HeaderDiv>
   );
 };
 
 const LogoView = styled.Image`
   width: 100%;
-  height: 50px;
-  margin: auto;
-  margin-bottom: 20px;
-  margin-top: 20px;
+  height: 100%;
 `;
-
+const ViewDiv = styled.View`
+  width: 260px;
+  height: 80px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const HomeButt = styled.TouchableOpacity`
+  width: 40px;
+  height: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: white;
+  position: absolute;
+  left: -50px;
+  border-radius: 10px;
+  top: 20px;
+`;
+const HeaderDiv = styled.View`
+  width: 100%;
+  height: 100px;
+  flex-direction: row;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+`;
 export default Header;
