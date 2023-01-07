b559c6fe6a1e8864b1dcf13f1eb4429326bf4601
maksymilianCzadowski <maksymilian.czadowski@gmail.com>
2023-01-07 22:14:03 +0100

    motus generic

--- App.js
+++ App.js
@@ -1,8 +1,15 @@
 import React from 'react';
-import {View, Text} from 'react-native';
 import Routes from './src/config/routes';
+import Sound from 'react-native-sound';
 
 const App = () => {
+  const sound = new Sound(require('./assets/motus-generic.mp3'), error => {
+    if (error) {
+      console.log('failed to load the sound', error);
+      return;
+    }
+    sound.play();
+  });
   return <Routes />;
 };
 
